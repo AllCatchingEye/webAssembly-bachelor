e76b415f87495829e7d7468f35d54a74b10078a0
Georg <langgeorg61@gmail.com>
2024-06-11 11:54:41 +0200

    Implemented c example component

--- wit-Interfaces/c_example/lib-component/add.c
+++ wit-Interfaces/c_example/lib-component/add.c
@@ -1,5 +1,8 @@
 #include "example.h"
+// #include "stdio.h"
 
 int32_t exports_example_component_adder_add(int32_t x, int32_t y) {
-  return x + y;
+  int result = x + y;
+  // printf("Result of %d + %d: %d", x, y, result);
+  return result;
 }
